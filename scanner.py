e7da0b626b0110d536934639928ecdb1db887256
my9143434 <my9143434@gmail.com>
2019-08-06 16:43:42 -0700

    General scanner

--- scanner.py
+++ scanner.py
@@ -2,6 +2,7 @@
 import requests
 import urlparse
 import re
+from BeautifulSoup import BeautifulSoup
 
 
 class Scanner:
@@ -31,3 +32,56 @@ class Scanner:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+        parsed_html = BeautifulSoup(response.content)
+        return parsed_html.findAll("form")
+
+    def submit_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urlparse.urljoin(url, action)
+        method = form.get("method")
+
+        inputs_list = form.findAll("input")
+        post_data = {}
+        for input in inputs_list:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = value
+
+            post_data[input_name] = input_value
+        if method == "post":
+            return self.session.post(post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
+
+    def run_scanner(self):
+        for link in self.target_links:
+            forms = self.extract_forms(link)
+            for form in forms:
+                print("[+] Testing form in " + link)
+                is_vulnerable_to_xss = self.test_xss_in_form(form, link)
+                if is_vulnerable_to_xss:
+                    print("\n\n[***] XSS discovered in " + link + "in the following form")
+                    print(form)
+
+            if "=" in link:
+                print("[+] Testing " + link)
+                is_vulnerable_to_xss = self.test_xss_in_link(link)
+                if is_vulnerable_to_xss:
+                    print("\n\n[***] Discovered XSS in " + link)
+
+    def test_xss_in_link(self, url):
+        xss_test_script = "<sCript>alert('hahaha')</scRipt>"
+        url = url.replace("=", "=" + xss_test_script)
+        response = self.session.get(url)
+
+        return xss_test_script in response.content
+
+    def test_xss_in_form(self, form, url):
+        xss_test_script = "<sCript>alert('hahaha')</scRipt>"
+        response = self.submit_form(form, xss_test_script, url)
+
+        return xss_test_script in response.content
